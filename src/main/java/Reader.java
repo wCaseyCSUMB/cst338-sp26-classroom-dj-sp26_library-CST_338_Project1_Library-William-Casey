a84474115606e527eb2b12da074f40570c3ca9b4
William Casey <wcasey@csumb.edu>
2026-03-24 13:34:27 -0700

    Reader final + header

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -3,11 +3,16 @@ import Utilities.Code;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author William Casey
+ * Abstract: The reader class works in tandem with the previous Book class, storing
+ * them in a list, allowing the adding and removing the books at will
+ */
 public class Reader {
-    public static final int CARD_NUMBER_;
-    public static final int NAME_;
-    public static final int PHONE_;
-    public static final int BOOK_COUNT_;
+    public static final int CARD_NUMBER_ = 3;
+    public static final int NAME_ = 0;
+    public static final int PHONE_ = 1;
+    public static final int BOOK_COUNT_ = 2;
     public static final int BOOK_START_ = 4;
     private int cardNumber;
     private String name;
@@ -22,15 +27,28 @@ public class Reader {
     }
 
     public Code addBook(Book bookIn) {
-
+        if(hasBook(bookIn)) {
+            return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+        }
+        books.add(bookIn);
+        return Code.SUCCESS;
     }
 
     public Code removeBook(Book bookIn) {
-
+        if(! hasBook(bookIn)) {
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+        }
+        books.remove(bookIn);
+        return Code.SUCCESS;
     }
 
     public boolean hasBook(Book bookIn) {
-
+        for(int i = 0; i < books.size(); i++) {
+            if(bookIn.equals(books.get(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<Book> getBooks() {
@@ -70,14 +88,26 @@ public class Reader {
     }
 
     public boolean equals(Object object) {
-
+        if(this == object) {
+            return true;
+        }
+        Reader compare = (Reader) object;
+        return cardNumber == compare.getCardNumber() && name.equals(compare.getName()) && phone.equals(compare.getPhone());
     }
 
     public int hashCode() {
-
+        int hash = Integer.hashCode(cardNumber);
+        hash = 31 * hash + name.hashCode();
+        hash = 31 * hash + phone.hashCode();
+        return hash;
     }
 
     public String toString() {
-        return name + " (#" + cardNumber + ") has checked out {";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < books.size(); i++) {
+            sb.append(books.get(i).toString());
+            sb.append(", ");
+        }
+        return name + " (#" + cardNumber + ") has checked out {" + sb.toString() + "}";
     }
 }
